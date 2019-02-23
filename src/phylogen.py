573d2f44587b6805a0f37b20a9d3e242e35545f1
David Mulder <dmulder@suse.com>
2019-02-23 16:11:21 -0700

    Provide fas directory, and locate all fas files

--- src/phylogen.py
+++ src/phylogen.py
@@ -2,6 +2,16 @@
 from subprocess import Popen, PIPE
 import sys, argparse, os
 from shutil import which
+from glob import glob
+
+def generate_trees(fas_dir):
+    fas_files = []
+    g = '*.fas'
+    globs = [g, g.title(), g.upper()]
+    for g in globs:
+        fas_files.extend(glob(os.path.join(fas_dir, g)))
+    for f in fas_files:
+        print(os.path.basename(f))
 
 if __name__ == '__main__':
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -9,10 +19,12 @@ if __name__ == '__main__':
     parser.add_argument('-s', help='Input directory of alignments in PHYLIP/FASTA/NEXUS/CLUSTAL/MSF format')
     parser.add_argument('--iqtree', help='Path to iqtree executable', default=os.path.join(current_dir, 'iqtree'))
     parser.add_argument('--astral', help='Path to astral jar executable', default=os.path.join(current_dir, 'astral.jar'))
+    parser.add_argument('fasdir', help='Directory containing files in fas format')
 
     args = parser.parse_args()
     iqtree = args.iqtree
     astral = args.astral
+    fas_dir = args.fasdir
 
     if not os.path.exists(iqtree):
         iqtree = which('iqtree')
@@ -23,7 +35,17 @@ if __name__ == '__main__':
         astral = which('astral.jar')
     if not astral or not os.path.exists(astral):
         print('Path to astral not found.\nEither add astral to your path, or specify it\'s location via the --astral parameter.')
-        exit(1)
+        exit(2)
 
     Popen(['iqtree -version | grep "version"'], shell=True).wait()
     Popen(['%s -jar %s 2>&1 | grep "version"' % (which('java'), args.astral)], shell=True).wait()
+
+    if not os.path.exists(fas_dir):
+        print('The specified fas dir "%s" does not exist' % fas_dir)
+        exit(3)
+    if not os.path.isdir(fas_dir):
+        print('The specified fas dir "%s" is not a directory' % fas_dir)
+        exit(4)
+
+    generate_trees(os.path.abspath(fas_dir))
+
