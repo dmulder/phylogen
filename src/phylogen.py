3209176a33a6439ff225b5d7f3988e6fb0020ff0
David Mulder <dmulder@suse.com>
2019-06-24 13:59:52 -0600

    Start using jpype to call astral

--- src/phylogen.py
+++ src/phylogen.py
@@ -4,9 +4,11 @@ import sys, argparse, os
 from shutil import which
 from glob import glob
 from tempfile import NamedTemporaryFile
-from multiprocessing import cpu_count
+from multiprocessing import cpu_count, Pool
 import numpy as np
 import re
+import jpype
+import jpype.imports
 
 size = 1
 rank = 0
@@ -63,16 +65,25 @@ def iqtree_trees(iqtree, fas_dir, output_dir, cores, cores_per_instance):
         ps.append(generate_tree(iqtree, f, out_file_prefix, threads))
     for p in ps:
         p.wait()
-    return tree_prefixes
+    tree_files = []
+    for tree_prefix in tree_prefixes:
+        g = '*.treefile'
+        globs = [g, g.title(), g.upper()]
+        for g in globs:
+            tree_files.extend(glob(os.path.join(tree_prefix, g)))
+    return tree_files
 
-def astral_tree(astral, input_file):
-    cmd = [which('java'), '-jar', astral, '-i', input_file]
-    devnull = open(os.devnull, 'w')
-    p = Popen(cmd, stderr=devnull, stdout=PIPE)
-    return p
+def astral_tree(args):
+    astral, input_file, output_file = args
+    jpype.startJVM(jpype.getDefaultJVMPath(), '-Djava.class.path=%s' % astral)
+    jpype.imports.registerDomain('phylonet')
+    from phylonet.coalescent import CommandLine
+    CommandLine.main(['-i', input_file, '-o', output_file])
+    jpype.shutdownJVM()
 
-def rank_treefile(data):
-    supports = [float(d) for d in re.findall(r'(\d+\.*\d*):\d+\.*\d*', data.decode())]
+def rank_treefile(filename):
+    data = open(filename, 'r').read().strip()
+    supports = [float(d) for d in re.findall(r'(\d+\.*\d*):\d+\.*\d*', data)]
     return sum(supports)/len(supports)
 
 def find_matching_fas_file(treefile, fas_dir):
@@ -98,23 +109,18 @@ def exon_length(treefile, fas_dir):
 
 best_rank = 0
 best_output = ''
-def validate(p, start, end):
+def validate(output_file):
     global best_rank, best_output
-    if p.poll() is not None:
-        tree = p.communicate()[0].strip()
-        rank = rank_treefile(tree)
-        if rank > best_rank:
-            print('Found a better rank of %f in range %d - %d' % (rank, start, end))
-            print('Best tree found so far:\n%s' % tree)
-            best_rank = rank
-            best_output = [tree]
-        elif rank == best_rank:
-            print('Found the same rank of %f in range %d - %d' % (rank, start, end))
-            best_output.append(tree)
-        else:
-            print('Found a worse rank of %f in range %d - %d' % (rank, start, end))
-        return True
-    return False
+    rank = rank_treefile(output_file)
+    if rank > best_rank:
+        print('Found a better rank of %f in %s' % (rank, output_file))
+        best_rank = rank
+        best_output = [output_file]
+    elif rank == best_rank:
+        print('Found the same rank of %f in %s' % (rank, output_file))
+        best_output.append(best_output)
+    else:
+        print('Found a worse rank of %f in %s' % (rank, output_file))
 
 if __name__ == '__main__':
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -126,6 +132,7 @@ if __name__ == '__main__':
     parser.add_argument('--cores-per-instance', help='The number of cores to utilize per iqtree instance', default=1)
     parser.add_argument('--outdir', help='A path to store the iqtree outputs', default=current_dir)
     parser.add_argument('--output', help='a filename for storing the output species tree.', default=os.path.join(current_dir, 'astral.treefile'))
+    parser.add_argument('--start', help='Index to start from (if restarting job)', default=0)
     parser.add_argument('fasdir', help='Directory containing files in fas format')
 
     args = parser.parse_args()
@@ -160,26 +167,29 @@ if __name__ == '__main__':
     tree_files = iqtree_trees(iqtree, os.path.abspath(fas_dir), os.path.abspath(output_dir), args.cores, args.cores_per_instance)
     # Only the main node will process the trees via astral
     if rank == 0:
-        tree_files.sort(key=lambda k : exon_length(k, args.fas_dir))
-        procs = int(args.cores)-1
-        ps = []
+        tree_files.sort(key=lambda k : exon_length(k, fas_dir))
+        input_files = []
+        output_files = []
         for i in range(int(args.start), len(tree_files)):
-            while len(ps) == procs:
-                for p, start, end, tmp_file in ps:
-                    if validate(p, start, end):
-                        os.remove(tmp_file)
-                        ps.remove((p, start, end, tmp_file))
+            output_file = os.path.join(current_dir, 'tmp_%dto%d.treefile' % (i, len(tree_files)))
+            try:
+                os.remove(output_file)
+            except FileNotFoundError:
+                pass
             with NamedTemporaryFile('w', dir=output_dir, delete=False) as t:
                 for treefile in tree_files[i:]:
                     t.write(open(treefile, 'r').read())
-
-                print('Calling astral on trees indexed from %d to %d' % (i, len(tree_files)))
-                p = astral_tree(args.astral, t.name)
-                ps.append((p, i, len(tree_files), t.name))
-        while len(ps) > 0:
-            for p, start, end, tmp_file in ps:
-                if validate(p, start, end):
-                    os.remove(tmp_file)
-                    ps.remove((p, start, end, tmp_file))
+                input_files.append(t.name)
+                output_files.append(output_file)
+        ps = Pool(processes=min(int(args.cores)-1, len(input_files)))
+        ps.map(astral_tree, zip([args.astral]*len(input_files), input_files, output_files))
+        for output_file in output_files:
+            validate(output_file)
         print('Best rank was %f with output in %s' % (best_rank, str(best_output)))
-
+        for cleanup_file in input_files + output_files:
+            if cleanup_file in best_output:
+                continue
+            try:
+                os.remove(cleanup_file)
+            except FileNotFoundError:
+                pass
