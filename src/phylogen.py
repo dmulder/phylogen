287dc81a71cd80fcd1eb21d0c7e6824a822f977a
David Mulder <dmulder@suse.com>
2019-06-28 08:16:35 -0600

    Use MPIPoolExecutor if mpi is available

--- src/phylogen.py
+++ src/phylogen.py
@@ -4,65 +4,49 @@ import sys, argparse, os
 from shutil import which
 from glob import glob
 from tempfile import NamedTemporaryFile
-from multiprocessing import cpu_count, Pool
+from multiprocessing import cpu_count
 import numpy as np
 import re
 import jpype
 import jpype.imports
 import iqtree
 
-size = 1
-rank = 0
 try:
-    from mpi4py import MPI
-    comm = MPI.COMM_WORLD
-    size = comm.Get_size()
-    rank = comm.Get_rank()
+    from mpi4py.futures import MPIPoolExecutor as Pool
 except ImportError:
-    pass
+    from multiprocessing import Pool
 
 def generate_tree(args):
-    fas_file, out_file_prefix, threads = args
+    fas_file, out_file_prefix = args
     print('Processing %s has begun' % fas_file)
-    args = [sys.argv[0].encode(), b'-quiet', b'-s', fas_file.encode(), b'-m', b'TEST', b'-nt', b'AUTO', b'-ntmax', b'%d' % threads, b'-pre', out_file_prefix.encode(), b'-bb', b'1000']
+    args = [sys.argv[0].encode(), b'-quiet', b'-s', fas_file.encode(), b'-m', b'TEST', b'-nt', b'1', b'-pre', out_file_prefix.encode(), b'-bb', b'1000']
     iqtree.main_entry(len(args), args)
 
-def iqtree_trees(fas_dir, output_dir, cores, cores_per_instance):
+def iqtree_trees(fas_dir, output_dir, cores):
     output_subdir = os.path.join(output_dir, os.path.basename(fas_dir))
     if not os.path.exists(output_subdir):
         os.mkdir(output_subdir)
 
-    if rank == 0:
-        fas_files = []
-        g = '*.fas'
-        globs = [g, g.title(), g.upper()]
-        for g in globs:
-            fas_files.extend(glob(os.path.join(fas_dir, g)))
-        tree_prefixes = [os.path.join(output_subdir, os.path.splitext(os.path.basename(f))[0]) for f in fas_files]
-        fas_files_chunks = [list(chunk) for chunk in np.split(np.array(fas_files), size)]
-    else:
-        tree_prefixes = None
-        fas_files_chunks = None
-
-    if size == 1 and rank == 0: # Running on a single node does not require MPI
-        fas_files_chunk = fas_files_chunks[0]
-    else:
-        fas_files_chunk = comm.scatter(fas_files_chunks, root=0)
-
-    procs = int(int(cores)/int(cores_per_instance))
-    threads = int(cores_per_instance)
+    fas_files = []
+    g = '*.fas'
+    globs = [g, g.title(), g.upper()]
+    for g in globs:
+        fas_files.extend(glob(os.path.join(fas_dir, g)))
+    tree_prefixes = [os.path.join(output_subdir, os.path.splitext(os.path.basename(f))[0]) for f in fas_files]
 
     arg_list = []
-    while len(fas_files_chunk) > 0:
-        f = fas_files_chunk.pop()
+    while len(fas_files) > 0:
+        f = fas_files.pop()
         prefix = os.path.splitext(os.path.basename(f))[0]
         out_file_dir = os.path.join(output_subdir, prefix)
         if not os.path.exists(out_file_dir):
             os.mkdir(out_file_dir)
         out_file_prefix = os.path.join(out_file_dir, prefix)
-        arg_list.append((f, out_file_prefix, threads))
-    ps = Pool(processes=min(procs, len(arg_list)))
+        arg_list.append((f, out_file_prefix))
+    ps = Pool(min(int(cores)-1, len(arg_list)))
     ps.map(generate_tree, arg_list)
+    if getattr(ps, 'shutdown', None):
+        ps.shutdown()
     tree_files = []
     for tree_prefix in tree_prefixes:
         g = '*.treefile'
@@ -73,6 +57,7 @@ def iqtree_trees(fas_dir, output_dir, cores, cores_per_instance):
 
 def astral_tree(args):
     astral, input_file, output_file = args
+    print('ASTRAL processing %s has begun' % input_file)
     jpype.startJVM(jpype.getDefaultJVMPath(), '-Djava.class.path=%s' % astral, convertStrings=False)
     from java.lang import System
     from java.io import PrintStream, File
@@ -130,7 +115,6 @@ if __name__ == '__main__':
     parser.add_argument('-s', help='Input directory of alignments in PHYLIP/FASTA/NEXUS/CLUSTAL/MSF format')
     parser.add_argument('--astral', help='Path to astral jar executable', default=os.path.join(current_dir, 'astral.jar'))
     parser.add_argument('--cores', help='The number of cores to utilize', default=cpu_count())
-    parser.add_argument('--cores-per-instance', help='The number of cores to utilize per iqtree instance', default=1)
     parser.add_argument('--outdir', help='A path to store the iqtree outputs', default=current_dir)
     parser.add_argument('--output', help='a filename for storing the output species tree.', default=os.path.join(current_dir, 'astral.treefile'))
     parser.add_argument('--skip-trees', help='Skip treefile generation, assume they are already created in the output directory', action='store_true')
@@ -161,7 +145,7 @@ if __name__ == '__main__':
 
     if not args.skip_trees:
         print('Processing trees on %d nodes with %s cores' % (size, args.cores))
-        tree_files = iqtree_trees(os.path.abspath(fas_dir), os.path.abspath(output_dir), args.cores, args.cores_per_instance)
+        tree_files = iqtree_trees(os.path.abspath(fas_dir), os.path.abspath(output_dir), args.cores)
     else:
         output_subdir = os.path.join(os.path.abspath(output_dir), os.path.basename(os.path.abspath(fas_dir)))
         tree_files = []
@@ -169,35 +153,36 @@ if __name__ == '__main__':
         globs = [g, g.title(), g.upper()]
         for g in globs:
             tree_files.extend(glob(os.path.join(output_subdir, '**', g)))
-    # Only the main node will process the trees via astral
-    if rank == 0:
-        tree_files.sort(key=lambda k : exon_length(k, fas_dir))
-        input_files = []
-        output_files = []
-        for i in range(int(args.start), len(tree_files)):
-            output_file = os.path.join(current_dir, 'tmp_%dto%d.treefile' % (i, len(tree_files)))
-            try:
-                os.remove(output_file)
-            except FileNotFoundError:
-                pass
-            with NamedTemporaryFile('w', dir=output_dir, delete=False) as t:
-                for treefile in tree_files[i:]:
-                    t.write(open(treefile, 'r').read())
-                input_files.append(t.name)
-                output_files.append(output_file)
-        ps = Pool(processes=min(int(args.cores)-1, len(input_files)))
-        ps.map(astral_tree, zip([args.astral]*len(input_files), input_files, output_files))
-        for output_file in output_files:
-            validate(output_file)
-        if len(best_output) == 1:
-            os.rename(best_output[0], final_output_file)
-            print('Best rank was %f with output in %s' % (best_rank, final_output_file))
-        else:
-            print('Best rank was %f with output in %s' % (best_rank, str(best_output)))
-        for cleanup_file in input_files + output_files:
-            if cleanup_file in best_output:
-                continue
-            try:
-                os.remove(cleanup_file)
-            except FileNotFoundError:
-                pass
+
+    tree_files.sort(key=lambda k : exon_length(k, fas_dir))
+    input_files = []
+    output_files = []
+    for i in range(int(args.start), len(tree_files)):
+        output_file = os.path.join(current_dir, 'tmp_%dto%d.treefile' % (i, len(tree_files)))
+        try:
+            os.remove(output_file)
+        except FileNotFoundError:
+            pass
+        with NamedTemporaryFile('w', dir=output_dir, delete=False) as t:
+            for treefile in tree_files[i:]:
+                t.write(open(treefile, 'r').read())
+            input_files.append(t.name)
+            output_files.append(output_file)
+    ps = Pool(min(int(args.cores)-1, len(input_files)))
+    ps.map(astral_tree, zip([args.astral]*len(input_files), input_files, output_files))
+    if getattr(ps, 'shutdown', None):
+        ps.shutdown()
+    for output_file in output_files:
+        validate(output_file)
+    if len(best_output) == 1:
+        os.rename(best_output[0], final_output_file)
+        print('Best rank was %f with output in %s' % (best_rank, final_output_file))
+    else:
+        print('Best rank was %f with output in %s' % (best_rank, str(best_output)))
+    for cleanup_file in input_files + output_files:
+        if cleanup_file in best_output:
+            continue
+        try:
+            os.remove(cleanup_file)
+        except FileNotFoundError:
+            pass
